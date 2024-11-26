3b1bcc58246364db8c435aa68e65c081cea8fef0
Akif Hadziabdic <akifh@spark-tech.io>
2024-11-26 21:44:18 +0100

    Form

--- storybook/stories/primitives/form/Form.tsx
+++ storybook/stories/primitives/form/Form.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import {Field, Form} from "@sparkui/react-form";
+import {Form} from "@sparkui/react-form";
 import styled from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const Example = () =>  {
-  const [nameEnabled, setNameEnabled] = React.useState<boolean>(true);
+export const FormStory = () =>  {
+  const [nameEnabled, setNameEnabled] = React.useState(true);
   const value = {personal: {name: '', description: "akif", age: 22}};
 
-  const name: Field<string> = {
+  const name = {
     name: "name",
     required: true,
     placeholder: "Name",
     param: "personal.name",
   }
 
-  const description: Field<string> = {
+  const description = {
     name: "description",
     required: true,
     pattern: /^[0-9\-+\/?]+$/,
@@ -21,7 +22,7 @@ export const Example = () =>  {
     param: "personal.description",
   }
 
-  const age: Field<number> = {
+  const age = {
     name: "age",
     required: false,
     placeholder: "age",
@@ -37,12 +38,14 @@ export const Example = () =>  {
               {({onChange, onBlur, error, element}) => (
                 <FormControl>
                   <input
+                    className="form-control"
+                    placeholder={name.name}
                     onChange={({target: {value}}) => onChange(value)}
                     onBlur={({target: {value}}) => onBlur(value)}
                     {...element}
                   />
-                  {error && <ErrorMessage>Validation failed</ErrorMessage>}
-                  <Button onClick={() => setNameEnabled(false)}>Hide name</Button>
+                  {error && <span className="alert alert-danger">Validation failed</span>}
+                  <button className="btn btn-secondary" onClick={() => setNameEnabled(false)}>Hide name</button>
                 </FormControl>
               )}
             </Form.TextField>
@@ -51,13 +54,15 @@ export const Example = () =>  {
         <Form.TextField {...description}>
           {({onChange, onBlur, error, element}) => (
             <FormControl>
-              <span>Digits and special characters only: -+/?</span>
+              <span className="form-label">Digits and special characters only: -+/?</span>
               <input
+                className="form-control"
+                placeholder={description.name}
                 onChange={({target: {value}}) => onChange(value)}
                 onBlur={({target: {value}}) => onBlur(value)}
                 {...element}
               />
-              {error && <ErrorMessage>Validation failed</ErrorMessage>}
+              {error && <span className="alert alert-danger">Validation failed</span>}
             </FormControl>
           )}
         </Form.TextField>
@@ -65,17 +70,19 @@ export const Example = () =>  {
           {({onChange, onBlur, error, element}) => (
             <FormControl>
               <input
+                className="form-control"
+                placeholder={age.name}
                 onChange={({target: {valueAsNumber}}) => onChange(valueAsNumber)}
                 onBlur={({target: {valueAsNumber}}) => onBlur(valueAsNumber)}
                 type="number"
                 {...element}
               />
-              {error && <ErrorMessage>Validation failed</ErrorMessage>}
+              {error && <span className="alert alert-danger">Validation failed</span>}
             </FormControl>
           )}
         </Form.NumericField>
         <Form.ButtonSubmit onSubmit={async (e) => console.log(e)}>
-          {(props) => (<Button {...props}>Submit</Button>)}
+          {(props) => (<button className="btn btn-primary"  {...props}>Submit</button>)}
         </Form.ButtonSubmit>
       </Form>
     </App>
@@ -93,12 +100,4 @@ const FormControl = styled.div`
     display:flex;
     flex-direction: column;
     gap: 4px;
-`;
-
-const ErrorMessage = styled.span`
-    color: red;
-`;
-
-const Button = styled.button`
-    background: white;
 `;
